dce34df223193da37f86f6260f0492c469c523a6
danielangelarro <danielangelarro@gmail.com>
2023-10-29 00:54:44 -0400

    Move files to Common Folder

--- src/GalleryApi.Infrastructure/DependencyInjection.cs
+++ src/GalleryApi.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using GalleryApi.Application.Common.Interfaces;
 using GalleryApi.Application.Common.Interfaces.Repository;
+using GalleryApi.Infrastructure.Authentication;
 using GalleryPhoto.Infrastructure.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,8 +8,12 @@ namespace GalleryApi.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        Microsoft.Extensions.Configuration.ConfigurationManager configuration)
     {
+        services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
+
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IPhotoRepository, PhotoRepository>();
 
