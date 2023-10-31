191417230f3a81a325539f6e5b0c79a109fa2d53
danielangelarro <danielangelarro@gmail.com>
2023-10-30 23:50:28 -0400

    add Fluent Validation

--- src/GalleryApi.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
+++ src/GalleryApi.Application/Authentication/Commands/Register/RegisterCommandHandler.cs
@@ -4,6 +4,7 @@ using GalleryApi.Application.Common.Interfaces.Authentication;
 using GalleryApi.Application.Common.Interfaces.Repository;
 using GalleryApi.Application.Entities;
 using GalleryApi.Application.DTO.Authentication;
+using FluentValidation;
 using ErrorOr;
 using MediatR;
 
@@ -13,15 +14,24 @@ public class RegisterCommandHandler : IRequestHandler<RegisterCommand, ErrorOr<A
 {
     private readonly IJwtTokenGenerator _jwtTokenGenerator;
     private readonly IUserRepository _userRepository;
+    private readonly IValidator<RegisterCommand> _validator;
 
-    public RegisterCommandHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator)
+    public RegisterCommandHandler(IUserRepository userRepository, IJwtTokenGenerator jwtTokenGenerator, IValidator<RegisterCommand> validator)
     {
         _userRepository = userRepository;
         _jwtTokenGenerator = jwtTokenGenerator;
+        _validator = validator;
     }
 
     public async Task<ErrorOr<AuthenticationResult>> Handle(RegisterCommand command, CancellationToken cancellationToken)
     {
+        var validationResult = await _validator.ValidateAsync(command);
+
+        if (!validationResult.IsValid)
+        {
+            return Errors.Model.ModelsInvalid;
+        }
+
         if (await _userRepository.GetUserByEmail(command.Email) is not null)
         {
             return Errors.User.DuplicateEmail;
